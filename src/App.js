72ba5868af3328117e9e95ed6922d6051180f027
User <diduhs0@gmail.com>
2025-02-04 20:28:02 +0200

    Add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
+import MovieDetails from "./pages/MovieDetails/MovieDetails";
+import MoviesPage from "./pages/MoviesPage/MoviesPage";
+import AdminPage from "./pages/AdminPage/AdminPage";
+
+
 import "./App.css";
 import Aside from "./components/Aside/Aside";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import Sessions from "./pages/Sessions/Sessions";
-import MovieDetails from "./pages/MovieDetails/MovieDetails";
 import Main from "./pages/Main/Main";
-import Widget from "./pages/Widget/Widget";
 
 const App = () => {
   return (
     <Router>
         <Aside />
+
           <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/sessions" element={<Sessions />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} />
-          <Route path="/widget/:movieId/seatplan" element={<Widget/>}/>
+          <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/" element={<Main />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/sessions" element={<Sessions />} />
+            <Route path="/movies/:movieId" element={<MovieDetails />} />
           </Routes>
     </Router>
   );
